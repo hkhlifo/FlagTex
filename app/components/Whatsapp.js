322c98353ce9fec3f695811834397ae3619fc3fa
hkhlifo <knightrideraa02@gmail.com>
2025-10-20 23:34:47 +0530

    Fixed some minor issues

--- app/components/Whatsapp.js
+++ app/components/Whatsapp.js
@@ -12,7 +12,7 @@ const WhatsApp = () => {
             className="fixed bottom-6 left-6 z-50 bg-[#25D366] text-white p-4 rounded-full shadow-xl hover:scale-105 hover:shadow-[0_0_20px_#25D366] transition-all duration-300 animate-bounce group"
         >
             {/* Tooltip */}
-            <span className="absolute right-full mr-3 top-1/2 -translate-y-1/2 bg-[#23272f] text-white text-xs px-3 py-1 rounded-md shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap">
+            <span className="absolute left-full mr-3 top-1/2 -translate-y-1/2 bg-[#23272f] text-white text-xs px-3 py-1 rounded-md shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap">
                 Chat with FlangTex
             </span>
 
